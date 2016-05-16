4cc145b311cc7aa0b0a6aa843c725458587d74b9
mohdhamzakhan <mohdhamzakhan@gmail.com>
2016-05-16 18:45:36 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,14 @@
+package myApps;
+
+public class Assignment1 
+{
+	public static void main(String[] args)
+	{
+		int[] myArray={3,4,1,5,7,2,77,86,56,90};
+		
+		for(int i=myArray.length-1;i>=0;i--)
+		{
+			System.out.println(myArray[i]);
+		}
+	}
+}
